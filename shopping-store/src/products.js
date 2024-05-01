1a3a1437ab9a5f9517c6637a60e41b4a55070774
adhamessam99 <adhamess89@gmail.com>
2024-05-01 22:18:26 +0300

    no Updates

---
+++ shopping-store/src/products.js
@@ -0,0 +1,71 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from ".//assets/shoes.webp";
+import product5 from "./assets/appleWatch.webp";
+import product6 from "./assets/bag.avif";
+import product7 from "./assets/7.webp";
+import product8 from "./assets/8.webp";
+import product9 from "./assets/hoodie.jpg";
+
+
+export const PRODUCTS=[
+    {
+    id:1,
+    productName:"Iphone",
+    price:999,
+    productImage:product1,
+    },
+    {
+        id: 2   ,
+        productName: "Macbook Pro 2022 ",
+        price: 1999.0,
+        productImage: product2,
+      },
+      {
+        id: 3,
+        productName: "Cannon M50 Camera",
+        price: 699.0,
+        productImage: product3,
+      },
+      {
+        id: 4,
+        productName: "Tommy Sneaker",
+        price: 130.0,
+        productImage: product4,
+      },
+      {
+        id: 5,
+        productName: "apple watch series 6",
+        price: 299.0,
+        productImage: product5,
+      },
+      {
+        id: 6,
+        productName: "Versace Bag",
+        price: 199.0,
+        productImage: product6,
+      },
+      {
+        id: 7,
+        productName: "AUTO SERVICE SHIRT by GOLF WANG",
+        price: 120.0,
+        productImage: product7,
+      },
+      {
+        id: 8,
+        productName: "DON'T TRIP UNSTRUCTURED HAT",
+        price: 40.0,
+        productImage: product8,
+      },
+      {
+        id: 9,
+        productName: "Lacoste Hoodie",
+        price: 90.0,
+        productImage: product9,
+      }
+
+
+
+
+];
